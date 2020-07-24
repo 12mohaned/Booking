0e59ddec6a912274e07e1fda21f9f9adb927a008
mohaned-mashaly <mohaned_boss@outlook.com>
2020-07-24 20:02:28 +0200

    Refactor Tests

--- main/pytest.py
+++ main/pytest.py
@@ -1,74 +1,76 @@
 import pytest
 from .models import  Buyer, Seller, Service, Booking, Inquiry,Seller_Service
+from django.urls import reverse
+from django import urls
+from django.test import TestCase
+from main.models import Buyer, Seller, Service, Booking, Inquiry, Seller_Service
 
-@pytest.mark.django_db
-def test_Buyer_model():
-    buyer = Buyer(username = 'mohaned.mashaly',
+@pytest.fixture
+def buyer():
+    return Buyer.objects.create(username = 'mohaned.mashaly',
                   firstname = 'mohaned',
                   lastname = 'mashaly',
                   email = 'mohaned.mashaly12@gmail.com')
+
+@pytest.fixture
+def seller():
+    return Seller.objects.create(username = 'mohaned.mashaly',Email = 'mohaned')
+
+@pytest.fixture
+def service(seller):
+    return Service.objects.create(name = 'Advertising Agency', content = 'a very good service.....', service_provider = seller)
+
+@pytest.fixture
+def booking(seller,buyer,service):
+    return Booking.objects.create(Sellername = seller,Buyername = buyer,service = service, date = '2020-09-09')
+
+@pytest.fixture
+def seller_service(seller, service):
+    return Seller_Service.objects.create(sellername = seller,servicename = service)
+
+@pytest.fixture
+def inquiry():
+    return Inquiry.objects.create(Message = 'I had problem with.....')
+
+@pytest.mark.django_db
+def test_Buyer_model(buyer):
     buyer.save()
     assert buyer != None and Buyer.objects.count() == 1
 
 @pytest.mark.django_db
-def test_Seller_model():
-    seller = Seller(username = 'mohaned.mashaly',
-              Email = 'mohaned')
+def test_Seller_model(seller):
     seller.save()
     assert seller != None and Seller.objects.count() == 1
 
 @pytest.mark.django_db
-def test_Service_model():
+def test_Service_model(seller, service):
 
-    seller = Seller(username = 'mohaned.mashaly',Email = 'mohaned')
     seller.save()
-
-    service = Service(name = 'Advertising Agency',content = 'a very good service.....',service_provider = seller)
     service.save()
     assert service != None and Service.objects.count() == 1
 
-
 @pytest.mark.django_db
-def test_Booking_model():
-
-    seller = Seller(username = 'mohaned.mashaly',Email = 'mohaned')
+def test_Booking_model(seller,service,buyer,booking):
     seller.save()
-
-    service = Service(name = 'Advertising Agency',content = 'a very good service.....',service_provider = seller)
     service.save()
-
-    buyer = Buyer(username = 'mohaned.mashaly',
-              firstname = 'mohaned',
-              lastname = 'mashaly',
-              email = 'mohaned.mashaly12@gmail.com')
     buyer.save()
-
-    booking = Booking(Sellername = seller,
-                      Buyername = buyer,
-                      service   = service,
-                      date = '2020-09-09')
     booking.save()
     assert booking != None and Booking.objects.count() == 1
 
 @pytest.mark.django_db
-def test_Inquiry_model():
-    size = Inquiry.objects.count()
-    inquiry = Inquiry(Message = 'I had problem with.....')
+def test_Inquiry_model(inquiry):
     inquiry.save()
     assert inquiry != None and Inquiry.objects.count() == 1
 
 @pytest.mark.django_db
-def test_Seller_Service_model():
-    seller = Seller(username = 'mohaned.mashaly',Email = 'mohaned')
+def test_Seller_Service_model(seller, service, seller_service):
     seller.save()
-
-    service = Service(name = 'Advertising Agency',
-                      content = 'a very good service.....',
-                      service_provider = seller)
     service.save()
-
-    seller_service = Seller_Service(
-              sellername = seller,
-              servicename = service)
     seller_service.save()
     assert seller_service != None and Seller_Service.objects.count() == 1
+
+@pytest.mark.django_db
+def test_home(client):
+    url = urls.reverse('main:Home')
+    response = client.get(url)
+    assert response.status_code == 200
